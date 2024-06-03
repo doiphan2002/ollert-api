2349ecef4ac10dcc75759a2f89ad91c2a3d5a998
pvdoi <phanvangioi1361@gmail.com>
2024-06-03 16:25:36 +0700

    Create Model

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -7,6 +7,7 @@
 import { StatusCodes } from 'http-status-codes'
 import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatter'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   try {
@@ -16,9 +17,16 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title)
     }
     // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
-    
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    // Lấy bản ghi board sau khi gọi (tùy mục đích dự án mà có cần bước này hay không)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+
+    // Làm thêm các xử lý logic với các Collection khác tùy đặc thù dự án...vv
+    // Bắn email, notification về cho admin khi có 1 cái board mới được tạo...vv
+
     // Trả kết quả về, trong Service luôn phải có return
-    return newBoard
+    return getNewBoard
   } catch (error) {throw error}
 
 }
